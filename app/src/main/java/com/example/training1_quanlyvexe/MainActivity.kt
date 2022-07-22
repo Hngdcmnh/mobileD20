ebebfc2435a81d45af8ba6b6c12822e292ef78b6
DungTheRipper <you@domain.com>
2022-07-22 16:12:25 +0700

    CDDung

--- app/src/main/java/com/example/training1_quanlyvexe/MainActivity.kt
+++ app/src/main/java/com/example/training1_quanlyvexe/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.training1_quanlyvexe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
+import com.example.training1_quanlyvexe.`object`.VeXe
+import com.example.training1_quanlyvexe.adapter.CustomAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val arr: ArrayList<VeXe> = ArrayList()
+        arr.add(VeXe("Nam Định - Hà Nội","29B-0536","5","19/07/2002"))
+        arr.add(VeXe("Vĩnh Phúc - Hà Nội","19A-0123","8","25/07/2002"))
+        arr.add(VeXe("Hà Nội - Vĩnh Phúc","20A-4567","16","10/08/2002"))
+        val gdvDSVe : GridView = findViewById(R.id.gdvDSVe)
+        gdvDSVe.adapter = CustomAdapter(this,arr)
     }
 }
--- app/src/main/java/com/example/training1_quanlyvexe/object/VeXe.kt
+++ app/src/main/java/com/example/training1_quanlyvexe/object/VeXe.kt
@@ -1,5 +1,5 @@
 package com.example.training1_quanlyvexe.`object`
 
-data class VeXe(val chuyenXe: String, val maXe: String, val soGhe: String, val ngayDiChuyen: String,) {
+data class VeXe(val tenXe: String, val maXe: String, val soGhe: String, val ngayDi: String,) {
 
 }
