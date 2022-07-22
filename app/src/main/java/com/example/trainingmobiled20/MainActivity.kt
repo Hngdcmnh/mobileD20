b365b903ea13a50346100170fd384834330bd4b4
Tuan Vo <dungtuan095@gmail.com>
2022-07-22 22:41:12 +0700

    First Commit

--- app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
+++ app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trainingmobiled20
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.id
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textSearch.visibility = View.INVISIBLE
     }
+
+    fun search(view: View) {
+        textSearch.visibility = View.VISIBLE
+        theTicket.visibility = View.INVISIBLE
+    }
+
+
 }
