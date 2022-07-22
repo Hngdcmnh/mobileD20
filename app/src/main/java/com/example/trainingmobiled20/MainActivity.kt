f9197f226c6cc81c65f7a46b0fe869ad4a810ced
VuNAP <vuvovan2k2>
2022-07-22 22:01:46 +0700

    Fix

--- app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
+++ app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.trainingmobiled20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search: ImageView = findViewById(R.id.imageView4)
+        val editTextSearch: EditText = findViewById(R.id.editTextSearch)
+        editTextSearch.isVisible = false
+        search.setOnClickListener(View.OnClickListener {
+            editTextSearch.isVisible = true
+        })
     }
 }
