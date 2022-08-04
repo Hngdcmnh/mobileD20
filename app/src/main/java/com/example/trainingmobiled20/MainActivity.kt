6dca12fa70e2a2e1ce3c91c2300d4bfb83e97654
S4ltF1sh <sankatana100@gmail.com>
2022-08-04 09:44:53 +0700

    first commit

--- app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
+++ app/src/main/java/com/example/trainingmobiled20/MainActivity.kt
@@ -1,44 +1,46 @@
-package com.example.searchbar
+package com.example.trainingmobiled20
 
-import android.app.Activity
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.searchbar.R
+import com.example.searchbar.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var hostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
-//        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolBar_ve_da_xuat)
-//        setSupportActionBar(toolbar)
-//
-//        toolbar?.inflateMenu(R.menu.menu_action_bar)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.veDaXuatFragment,
-                R.id.quetVeFragment,
-                R.id.khacFragment,
-                R.id.xuatVeFragment
-            )
-        )
-//
-        val hostFragment =
+
+        setupNavController()
+        setUpActionbar()
+    }
+
+    private fun setupNavController() {
+        hostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = hostFragment.navController
+    }
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bot_nav_view)
+    private fun setUpActionbar() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.demoFragment,
+                R.id.goFoodFragment
+            )
+        )
 
-        //setupActionBarWithNavController(navController)
-        bottomNavigationView.setupWithNavController(navController)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
 }
---
+++ app/src/main/java/com/example/trainingmobiled20/fragment/HomeFragment.kt
@@ -0,0 +1,45 @@
+package com.example.trainingmobiled20.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.searchbar.R
+import com.example.searchbar.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        setOnClick()
+
+        return binding.root
+    }
+
+    private fun setOnClick() {
+        binding.btnDemo.setOnClickListener {
+            goToDemoFragment()
+        }
+
+        binding.btnGoFood.setOnClickListener {
+            goToGoFoodFragment()
+        }
+    }
+
+    private fun goToDemoFragment() {
+        findNavController().navigate(R.id.action_homeFragment_to_demoFragment)
+    }
+
+    private fun goToGoFoodFragment() {
+        findNavController().navigate(R.id.action_homeFragment_to_goFoodFragment)
+
+    }
+}
