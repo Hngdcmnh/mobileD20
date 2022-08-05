5669f21526c3bff0a9c918a43bac9ab849441a50
Tuan Vo <dungtuan095@gmail.com>
2022-08-05 16:50:23 +0700

    First Commit

---
+++ app/src/main/java/com/example/trainingmobiled20/StudentAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.trainingmobiled20
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trainingmobiled20.databinding.ItemBinding
+
+class StudentAdapter(private val onItemClick:(Student,Int)-> Unit):RecyclerView.Adapter<StudentAdapter.ViewHolder>()  {
+    private val students = ArrayList<Student>()
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(students[position])
+    }
+
+    override fun getItemCount(): Int {
+        return students.size
+    }
+    class ViewHolder(private val binding: ItemBinding,private val onTitleClick:(Student,Int)->Unit):RecyclerView.ViewHolder(binding.root){
+        fun bindData(item:Student){
+            binding.apply {
+                tvNameStudent.text = item.name
+                tvYearStudent.text = item.year.toString()
+                btnStudent.setOnClickListener {
+                    onTitleClick(item,adapterPosition)
+
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= ViewHolder (
+        ItemBinding.inflate(LayoutInflater.from(parent.context),parent,false),onItemClick
+
+    )
+    fun setData(data:List<Student>){
+        students.clear()
+        students.addAll(data)
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/example/trainingmobiled20/StudentFragment.kt
+++ app/src/main/java/com/example/trainingmobiled20/StudentFragment.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.trainingmobiled20.databinding.FragmentStudentBinding
 
 class StudentFragment : Fragment() {
 
     private lateinit var binding: FragmentStudentBinding
-
+    private val adapter = StudentAdapter(this::onTitleClick)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStudentBinding.inflate(inflater,container,false)
-
+        setupData()
         return binding.root
     }
 
+    private fun setupData() {
+        val students = ArrayList<Student>()
+        for(i in 1..10){
+            students.add(Student("Học sinh thứ $i",2000+i))
+        }
+        binding.rvStudent.adapter = adapter
+        adapter.setData(students)
+    }
+    private fun onTitleClick(student: Student,position:Int){
+        Toast.makeText(context,"${student.name}", Toast.LENGTH_SHORT).show()
+        adapter.notifyItemChanged(position)
+    }
 
 }
+
+
